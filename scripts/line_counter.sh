51d7ae75c3a1f19022e5b80980d84d42a2bc72df
Liam Nelligan <liamnelligan000@gmail.com>
2023-10-31 16:48:57 +0100

    removed blank lines and comments

--- scripts/line_counter.sh
+++ scripts/line_counter.sh
@@ -8,7 +8,6 @@ fi
 
 # Set the directory to search in
 directory="$1"
-echo $directory
 
 # Remove the first argument (directory) to get the file extensions
 shift
@@ -20,10 +19,12 @@ total_lines=0
 for extension in "$@"; do
     # Use 'find' to locate files with the specified extension and count their lines
     while IFS= read -r -d '' file; do
-        lines=$(wc -l < "$file")
+        # Count non-empty and non-comment lines
+        lines=$(grep -c -vE '^\s*($|#|//)' "$file")
         total_lines=$((total_lines + lines))
     done < <(find "$directory" -type f -name "*.$extension" -print0)
 done
 
 # Print the total lines of code
-echo "Total lines of code: $total_lines"
+echo "Total lines of code (excluding comments and empty lines): $total_lines"
+
