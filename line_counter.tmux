18d93380947353c0ea35bbaec21b5d3c2707ce9a
Liam Nelligan <liamnelligan000@gmail.com>
2023-10-31 17:06:29 +0100

    ignore venv

--- line_counter.tmux
+++ line_counter.tmux
@@ -3,4 +3,4 @@
 CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 WORKING_DIR=$(pwd)
 FILE_ENDINGS="py"
-tmux bind-key T run-shell "$CURRENT_DIR/scripts/line_counter.sh $(tmux display-message -p '#{pane_current_path}') $FILE_ENDINGS"
+tmux bind-key T run-shell "$CURRENT_DIR/scripts/line_counter.sh $(tmux display-message -p '#{pane_current_path}') venv $FILE_ENDINGS"
--- scripts/line_counter.sh
+++ scripts/line_counter.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 
 # Check for correct number of arguments
-if [ "$#" -lt 2 ]; then
-    echo "Usage: $0 <directory> <file_extensions...>"
+if [ "$#" -lt 3 ]; then
+    echo "Usage: $0 <directory> <venv_name> <file_extensions...>"
     exit 1
 fi
 
 # Set the directory to search in
 directory="$1"
 
-# Remove the first argument (directory) to get the file extensions
-shift
+# Set the virtual environment name
+venv_name="$2"
+
+# Remove the first two arguments (directory and venv_name) to get the file extensions
+shift 2
 
 # Initialize a variable to store the total lines of code
 total_lines=0
@@ -19,12 +22,15 @@ total_lines=0
 for extension in "$@"; do
     # Use 'find' to locate files with the specified extension and count their lines
     while IFS= read -r -d '' file; do
-        # Count non-empty and non-comment lines
-        lines=$(grep -c -vE '^\s*($|#|//)' "$file")
-        total_lines=$((total_lines + lines))
+        # Check if the file is outside the virtual environment directory
+        if [[ "$file" != "$directory/$venv_name"* ]]; then
+            # Count non-empty lines (ignoring lines that contain only whitespace)
+            lines=$(grep -v '^[[:space:]]*$' "$file" | wc -l)
+            total_lines=$((total_lines + lines))
+        fi
     done < <(find "$directory" -type f -name "*.$extension" -print0)
 done
 
 # Print the total lines of code
-echo "Total lines of code (excluding comments and empty lines): $total_lines"
+echo "Total lines of code (excluding empty lines and files in $venv_name directory): $total_lines"
 
