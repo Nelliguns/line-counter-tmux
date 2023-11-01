4a7b271f46495a55d980f59fc071947e2b285cb2
Liam Nelligan <liamnelligan000@gmail.com>
2023-11-01 16:51:04 +0100

    -

--- scripts/line_counter.sh
+++ scripts/line_counter.sh
@@ -6,48 +6,28 @@ if [ $# -ne 2 ]; then
     exit 1
 fi
 
+# Assign the arguments to variables
 file_endings="$1"
-current_directory="$(tmux display-message -p '#{pane_current_path}')"
-directory_to_ignore="$2"
-lines_of_code=0
+folder_name="$2"
+start_dir="$(tmux display-message -p '#{pane_current_path}')"
 
-# Function to count lines of code in a file
-count_lines_of_code() {
-    local file="$1"
-    local lines=0
+# Initialize a variable to store the total lines of code
+total_lines=0
 
-    if [ -f "$file" ]; then
-        lines=$(wc -l < "$file")
-    fi
-
-    echo "$lines"
+# Function to process files
+process_file() {
+  file="$1"
+  lines=$(wc -l < "$file")
+  total_lines=$((total_lines + lines))
 }
 
-# Function to traverse the directory tree
-traverse_directory() {
-    local directory="$1"
-    local ignore_directory="$2"
+# Recursively find and process files while excluding directories with the specified folder name
+find "$start_dir" -type d -name "$folder_name" -prune -o -type f -print | while read -r file; do
+  # Check if the file matches the specified file endings
+  if echo "$file" | grep -Eq "\\.($file_endings)$"; then
+    process_file "$file"
+  fi
+done
 
-    for item in "$directory"/*; do
-        if [ "$item" == "$ignore_directory" ]; then
-            continue  # Skip the directory to ignore
-        elif [ -d "$item" ]; then
-            # If it's a directory, recursively call the function
-            traverse_directory "$item" "$ignore_directory"
-        elif [ -f "$item" ]; then
-            # If it's a file, check if it matches the specified file endings
-            if [[ "$item" == *.$file_endings ]]; then
-                # If it matches, count lines of code and add to the total
-                lines=$(count_lines_of_code "$item")
-                lines_of_code=$((lines_of_code + lines))
-            fi
-        fi
-    done
-}
-
-# Start the directory traversal
-traverse_directory "$current_directory" "$directory_to_ignore"
-
-# Return the total lines of code
-echo "$current_directory"
-echo "Total lines of code: $lines_of_code"
+# Display the total lines of code found
+echo "Total lines of code: $total_lines"
