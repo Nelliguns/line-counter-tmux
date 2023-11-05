71b8ae63ff865d51eedadc31ddc51e1d87f266b8
Chemaclass <chemaclass@outlook.es>
2023-11-05 12:42:48 +0100

    test_one_file_extension

--- scripts/line_counter.sh
+++ scripts/line_counter.sh
@@ -5,13 +5,6 @@ if [ "$#" -lt 2 ]; then
     echo "Usage: $0 <venv_name> <file_extensions>"
     exit 1
 fi
-
-# Set the virtual environment name
-venv_name="$1"
-
-# Split the provided file_extensions argument into an array
-IFS=',' read -ra file_extensions <<< "$2"
-
 # Set the directory to the current path of the tmux pane
 directory="$(tmux display-message -p '#{pane_current_path}')"
 
@@ -29,9 +22,9 @@ for extension in "${file_extensions[@]}"; do
     # Use 'find' to locate files with the specified extension and count their lines
     while IFS= read -r -d '' file; do
         # Check if the file is outside the virtual environment directory
-        if [[ "$file" != "$directory/$venv_name"* ]]; then
+        if [[ "$file" == "$directory/$venv_name"* ]]; then
             # Count non-empty lines (ignoring lines that contain only whitespace)
-            lines=$(grep -v '^[[:space:]]*$' "$file" | wc -l)
+            lines=$(grep -cv '^[[:space:]]*$' "$file")
             total_lines=$((total_lines + lines))
         fi
     done < <(find "$directory" -type f -name "*.$extension" -print0)
